89ace9a899fe12790bffd91fa8efb0b21c7f996c
WellingtonEduardo <wellingtoneduardo10@outlook.com>
2023-11-25 12:58:32 -0300

    I implemented the route for updating bank account details.

--- src/modules/bank-accounts/bank-accounts.service.ts
+++ src/modules/bank-accounts/bank-accounts.service.ts
@@ -1,5 +1,9 @@
 import { BankAccountsRepository } from './../../shared/database/repositories/bank-accounts.repository';
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
 import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
 
@@ -8,6 +12,14 @@ export class BankAccountsService {
   constructor(
     private readonly bankAccountsRepository: BankAccountsRepository,
   ) {}
+
+  findAllByUserId(userId: string) {
+    return this.bankAccountsRepository.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
 
   create(userId: string, createBankAccountDto: CreateBankAccountDto) {
     const { name, initialBalance, color, type } = createBankAccountDto;
@@ -22,16 +34,33 @@ export class BankAccountsService {
     });
   }
 
-  findAllByUserId(userId: string) {
-    return this.bankAccountsRepository.findMany({
+  async update(
+    userId: string,
+    bankAccountId: string,
+    updateBankAccountDto: UpdateBankAccountDto,
+  ) {
+    const isOwner = await this.bankAccountsRepository.findFirst({
       where: {
+        id: bankAccountId,
         userId,
       },
     });
-  }
 
-  update(id: number, updateBankAccountDto: UpdateBankAccountDto) {
-    return `This action updates a #${id} bankAccount`;
+    if (!isOwner) {
+      throw new NotFoundException('Bank account not found.');
+    }
+
+    const { name, initialBalance, color, type } = updateBankAccountDto;
+
+    return this.bankAccountsRepository.update({
+      where: { id: bankAccountId },
+      data: {
+        name,
+        initialBalance,
+        color,
+        type,
+      },
+    });
   }
 
   remove(id: number) {
--- src/shared/database/repositories/users.repository.ts
+++ src/shared/database/repositories/users.repository.ts
@@ -6,11 +6,11 @@ import { Prisma } from '@prisma/client';
 export class UsersRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
-  create(createDto: Prisma.UserCreateArgs) {
-    return this.prismaService.user.create(createDto);
-  }
-
   findUnique(findUniqueDto: Prisma.UserFindUniqueArgs) {
     return this.prismaService.user.findUnique(findUniqueDto);
+  }
+
+  create(createDto: Prisma.UserCreateArgs) {
+    return this.prismaService.user.create(createDto);
   }
 }
